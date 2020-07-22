2b5c1db7888cf6d04349a2a7b2123bc7cf28d753
madnesso <deadsasuke@gmail.com>
2020-07-22 20:33:29 +0200

    playing around with REST and MVDB API

--- app/src/main/java/com/example/movies/data/db/entity/MoviesEntry.kt
+++ app/src/main/java/com/example/movies/data/db/entity/MoviesEntry.kt
@@ -1,15 +1,15 @@
 package com.example.movies.data.db.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Movies")
+@Entity(tableName = "Movies", indices = [Index(value = ["title"], unique = true)])
 data class MoviesEntry(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val poster_path: String?,
     val overview: String,
     val release_date: String,
     val title: String
-) {
-    @PrimaryKey(autoGenerate = true)
-    var id: Int = 0
-}
+)
